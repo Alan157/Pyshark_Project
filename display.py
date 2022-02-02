7eeabc65c138c0fc4e3e1de0e0764be5eddb57f2
alan157 <alanthebest33@gmail.com>
2022-02-02 11:38:17 -0500

    Minor changes

--- display.py
+++ display.py
@@ -3,25 +3,9 @@ sys.path.append('/home/kali/PycharmProjects/pythonProject/venv/lib/python3.9/sit
 import pyshark
 
 
-def capture_live_packets(network_interface):
-    capture = pyshark.LiveCapture(interface=network_interface)
-    for raw_packet in capture.sniff_continuously():
-        print(raw_packet)
-
-
 """
-----Main program that will call the other scripts----
-1)* ###DONE### Monitors the network and capture relevant packets form unknown IPs (i.e excluding SOC IPs)
-  * ###DONE### Writes the relevant packets to a file (a simple txt file)
-  * ###DONE### Gives the option between capturing full packets and summaries (only_summaries = True/False)
-2)Backs up local machine logs every 2 or so minutes (/var/log directory)
-
-TO CHECK:
-*How to run a python script in a new CLI
-* ###DONE### Why running .py script didnt work through CLI (import error)
-*Close a CLI/running script
-*
-
+This program uses Pyshark to monitor the traffic on a specified network interface
+and writes the output to a text file for logging.
 """
 print("Welcome to the live network traffic feed:")
 while True:  # A loop to capture and validate the input of the interface variable
@@ -32,7 +16,7 @@ while True:  # A loop to capture and validate the input of the interface variabl
         break
 
 
-filters = input("\nPlease type with filters you would like to use(leave empty for all traffic):")  # An input for the filters variable
+filters = input("\nPlease type with filters you would like to use(leave empty for all traffic):")  # An input for the filters variable(bpf filter)
 
 while True:  # A loop to capture and validate the input of the summaries variable
     summaries = input("\nDisplay packets as summaries?(y/n):")
@@ -46,16 +30,16 @@ while True:  # A loop to capture and validate the input of the summaries variabl
 
 while True:  # A loop to capture and validate the input of the path variable
 
-    path = input("\nPlease type the path to which the logs will be saved:")
+    path = input("\nPlease type the path to which the logs will be saved(Default:/home/kali/Desktop/Logs/):")
     if path == "":
         "\nMust enter a valid path!"
     else:
         break
 
-print("Starting packet sniff and display")
+print("\n------------Starting packet sniff, display and logging------------")
 capture = pyshark.LiveCapture(interface=network_interface, only_summaries=summaries, bpf_filter=filters)  # starting the capture
 
-output = open(path, "w")  # Opens a file for writing logs
+output = open("/home/kali/Desktop/Logs/" + path, "w")  # Opens a file for writing logs
 
 for packet in capture.sniff_continuously():  # A loop for printing and writing the packets
     print(packet)  # Printing the packet
